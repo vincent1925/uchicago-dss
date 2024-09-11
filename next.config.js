4cc2eb75279a1479120e172b3197075a45d98669
markxiong0122 <markxiong0122@gmail.com>
2024-09-11 23:53:13 +0800

    Initial gh-pages commit

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig = {
+  images: {
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+    unoptimized: true, // Disable image optimization for GitHub Pages
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
+
+module.exports = nextConfig;
